e66dbe501e1961970a1c9d917718dd4c177203db
Alisson <alisson_bercalini@hotmail.com>
2019-10-21 19:27:24 -0300

    auto deploy

--- src/app/seguranca/httpErrorInterceptor .ts
+++ src/app/seguranca/httpErrorInterceptor .ts
@@ -21,6 +21,13 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           let errorMessage = '';
           console.log(error);
 
+          if (error.error.error === 'Internal Server Error' &&
+            error.error.message.inclues('could not execute statement; SQL [n/a]; constraint [null]') &&
+            error.status === 500) {
+              errorMessage = 'Esse registro já esta sendo movimentada por outra tabela';
+              this.messageService.add({severity: 'error', summary: errorMessage, detail: errorMessage});
+          }
+
           if (error.error.error === 'invalid_token' && error.error.error_description.includes('Access token expired:')) {
             errorMessage = 'Atualizar a pagina por favor';
             this.auth.obterNovoAccessToken().subscribe(() => null);
